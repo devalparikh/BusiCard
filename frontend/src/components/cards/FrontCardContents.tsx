3391df02ece581fab8b5c4514880733e34acefc2
Deval Parikh <devalpp@gmail.com>
2021-04-29 15:58:11 -0400

    husky

--- frontend/src/components/cards/FrontCardContents.tsx
+++ frontend/src/components/cards/FrontCardContents.tsx
@@ -1,49 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
-import BootstrapButton from 'react-bootstrap/Button';
-import PropTypes from 'prop-types';
-import ReactCardFlip from 'react-card-flip';
-import { iMainCard } from './MainCard';
+import BootstrapButton from "react-bootstrap/Button";
+import PropTypes from "prop-types";
+import ReactCardFlip from "react-card-flip";
+import { iMainCard } from "./MainCard";
 function FrontCardContents(props: iMainCard) {
+  const ProfilePicture = styled.img`
+    width: 120px;
+    height: 120px;
+    background: #ffffff;
+    border-radius: 50%;
+    margin: 20px;
+  `;
 
-    const ProfilePicture = styled.img`
-        width: 120px;
-        height: 120px;
-        background: #FFFFFF;
-        border-radius: 50%;
-        margin: 20px;
-    `;
+  const NameText = styled.h1`
+    font-weight: 700;
+    font-size: 20px;
+  `;
 
-    const NameText = styled.h1`
-        font-weight: 700;
-        font-size: 20px;
-    `;
+  const TitleText = styled.h1`
+    font-weight: 400;
+    font-size: 14px;
+  `;
 
-    const TitleText = styled.h1`
-        font-weight: 400;
-        font-size: 14px;
-    `;
+  const ContactInfoSection = styled.div`
+    margin: 60px 0px 20px 0px;
+  `;
 
-    const ContactInfoSection = styled.div`
-        margin: 60px 0px 20px 0px;
-    `;
+  const ContactInfo = styled.p`
+    color: dimgray;
+  `;
 
-    const ContactInfo = styled.p`
-        color: dimgray
-    `;
+  const AllContactInfo = props.contactInfo.map((contactInfo, index) => {
+    return <ContactInfo key={index}>{contactInfo}</ContactInfo>;
+  });
 
-    const AllContactInfo = props.contactInfo.map((contactInfo, index) => {
-        return (<ContactInfo key={index}>{contactInfo}</ContactInfo>);
-    });
-
-    return (
-        <>
-            <ProfilePicture src={props.imageURL} />
-            <NameText>{props.name}</NameText>
-            <TitleText>{props.title}</TitleText>
-            <ContactInfoSection>{AllContactInfo}</ContactInfoSection>
-        </>
-    );
+  return (
+    <>
+      <ProfilePicture src={props.imageURL} />
+      <NameText>{props.name}</NameText>
+      <TitleText>{props.title}</TitleText>
+      <ContactInfoSection>{AllContactInfo}</ContactInfoSection>
+    </>
+  );
 }
 
 export default FrontCardContents;
