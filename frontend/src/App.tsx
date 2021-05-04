32df54a6dbc4ca6e027d18eea9a4b8dbef81df77
Deval Parikh <devalpp@gmail.com>
2021-05-04 18:13:38 -0400

    clean up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Playground from "./containers/Playground";
-import { Switch, Route, useLocation } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Landing from "./containers/Landing";
 
 function App() {
