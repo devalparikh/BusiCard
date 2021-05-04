ee1e63474e3ce0de14319270d5b2b99589880a57
Deval Parikh <devalpp@gmail.com>
2021-05-04 18:08:44 -0400

    react router dom setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css';
-import Playground from './containers/Playground';
+import React from "react";
+import "./App.css";
+import Playground from "./containers/Playground";
+import { Switch, Route, useLocation } from "react-router-dom";
+import Landing from "./containers/Landing";
 
 function App() {
   return (
     <div className="App">
-      <Playground test={false}/>
+      <Switch>
+        <Route exact path="/">
+          {/* TODO: Pass in array of categories */}
+          <Landing />
+        </Route>
+        <Route exact path="/playground">
+          {/* TODO: Pass in array of categories */}
+          <Playground test={false} />
+        </Route>
+      </Switch>
     </div>
   );
 }
