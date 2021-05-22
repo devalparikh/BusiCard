36403cfb29885d3d58b0fb48f473dc5de6411c58
Jerome Steve Sahayadarlin <jsahayad@masonlive.gmu.edu>
2021-05-21 20:39:32 -0400

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Playground from './containers/Playground';
 import NavBar from './components/navbar/NavBar';
+import EditSidebar from './components/edit/EditSidebar';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-      <Playground test={false}/>
+      <EditSidebar></EditSidebar>
+      {/* <Playground test={false}/> */}
     </div>
   );
 }
---
+++ frontend/src/components/edit/EditSidebar.tsx
@@ -0,0 +1,66 @@
+import { Image, Form, Button } from 'react-bootstrap';
+import styled from "styled-components";
+
+const EditSidebarWrapper = styled.div`
+  width: 350px;
+  height: 87.5%;
+  padding: 40px;
+  
+  border: 0px solid #000000;
+  border-radius: 30px;
+  
+  box-shadow: 2px 2px 50px rgba(0, 0, 0, 0.2);
+  position: fixed;
+  
+`;
+
+const OuterPad = styled.div`
+  display: flex;
+  justify-content: left;
+  padding: 20px;
+`;
+
+
+const EditSidebar = () => {
+  return(
+    <OuterPad>
+      <EditSidebarWrapper>
+        <div>
+          <Image src="http://download.seaicons.com/icons/mahm0udwally/all-flat/128/User-icon.png" rounded />
+
+          <Form>
+
+            <Form.Group controlId="name">
+              <Form.Control placeholder="Name" />
+            </Form.Group>
+
+            <Form.Group controlId="header">
+              <Form.Control placeholder="Header" />
+            </Form.Group>
+
+            <br></br>
+            <br></br>
+
+            <Form.Group controlId="phone">
+              <Form.Control placeholder="Phone Number" />
+            </Form.Group>
+
+            <Form.Group controlId="email">
+              <Form.Control placeholder="Email (Optional)" />
+            </Form.Group>
+
+            <Form.Group controlId="address">
+              <Form.Control  placeholder="Address (Optional)" />
+            </Form.Group>
+
+            <Button variant="dark" type="submit" size="lg">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </EditSidebarWrapper>
+    </OuterPad>
+  )
+}
+
+export default EditSidebar;
