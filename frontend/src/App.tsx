b756e09d45ceb4b37bb666f382cff8a911f37930
Jerome Steve Sahayadarlin <jsahayad@masonlive.gmu.edu>
2021-05-25 20:33:32 -0400

    adding sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Playground from "./containers/Playground";
 import NavBar from "./components/navbar/NavBar";
+import EditSidebar from './components/edit/EditSidebar';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <EditSidebar></EditSidebar>
       <Playground test={false} />
     </div>
   );
